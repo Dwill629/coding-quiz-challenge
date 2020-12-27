ac6ef1a5fbdb4b14ee68e1fb882d4757bbd7b8e0
Devon <Dwill629@gmail.com>
2020-12-27 13:40:03 -0500

    added questions to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// list of all questions, choices, and answers
+var questionList = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      answers: ["strings", "booleans", "alerts", "numbers"],
+      correctAnswer: 3
+    },
+    {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      answers: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      correctAnswer: 3
+    },
+    {
+      question: "Arrays in JavaScript can be used to store ____.",
+      answers: [ "numbers and strings", "other arrays", "booleans", "all of the above"
+      ],
+      correctAnswer: 4
+    },
+    {
+      question: "String values must be enclosed within ____ when being assigned to variables.",
+      answers: ["commas", "curly brackets", "quotes", "parentheses"],
+      correctAnswer: 3
+    },
+    {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      answers: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      correctAnswer: 4
+    }
+  ];
